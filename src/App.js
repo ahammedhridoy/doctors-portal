b227de6ea6d21b589b235dcda1826bf23dec016e
Ashik Ahammed Hridoy <35147425+ahammedhridoy@users.noreply.github.com>
2022-12-05 00:11:58 +0600

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navabr from "./pages/Shared/Navbar/Navabr";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home/Home";
+import About from "./pages/About/About";
+import NotFound from "./pages/NotFound/NotFound";
+import Blog from "./pages/Blog/Blog";
+import ContactUs from "./pages/ContactUs/ContactUs";
+import Reviews from "./pages/Reviews/Reviews";
+import DentalServices from "./pages/DentalServices/DentalServices";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Navabr></Navabr>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="/contact-us" element={<ContactUs></ContactUs>}></Route>
+        <Route path="/Reviews" element={<Reviews></Reviews>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route
+          path="/dental-services"
+          element={<DentalServices></DentalServices>}
+        ></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/DentalServices/DentalServices.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DentalServices = () => {
+  return (
+    <div>
+      <h1>Dental Services</h1>
+    </div>
+  );
+};
+
+export default DentalServices;
+<h1>Dental Services</h1>;
